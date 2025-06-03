91f6a309e60147eaf077bf266da2a38fd0d17fe0
Daniela <danielamanzanorangel@email.com>
2025-06-03 10:54:51 -0600

    optimización del modulo agendas

--- routes/agenda.routes.js
+++ routes/agenda.routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 router.post('/agenda', AgendaController.registrarAgenda);
 router.get('/agenda', AgendaController.obtenerAgenda);
-router.get('/agenda/:coordinador', AgendaController.obtenerAgendasPorCoordinador);
 router.get('/domicilios', AgendaController.obtenerDomicilios);
 router.put('/agenda/:id', AgendaController.actualizarAgenda);
 router.delete('/agenda/:id', AgendaController.eliminarAgenda);
