860dd0fddb6b13d321eced36ef5d1978b31a6f4e
Winy <danielamanzanorangel@gmail.com>
2025-09-04 12:12:10 -0600

    url variables

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ conectarDB();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:4200', // tu frontend
+  origin: 'https://supervisor-operacion.web.app', // tu frontend
   credentials: true
 }));
 app.use(express.json());
