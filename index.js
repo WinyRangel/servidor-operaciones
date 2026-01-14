6c6c33c44e04be0f83eced40567e95ef506a03f5
Winy <danielamanzanorangel@gmail.com>
2026-01-14 16:44:29 -0600

    guardar agenda de asesores

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ conectarDB();
 // ];
 
 app.use(cors({
-  origin: 'https://supervisor-operacion.web.app', // tu frontend
+  origin: 'https://supervisor-operacion.web.app', // tu  frontend producccion https://supervisor-operacion.web.app
   credentials: true
 }));
 //https://supervisor-operacion.web.app
